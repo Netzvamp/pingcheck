04dd714e630cebcef30e81c2ab80e87f06de661a
Robert Lieback <robertlieback@gmail.com>
2020-10-06 12:18:56 +0200

    optimisations

--- pingcheck.py
+++ pingcheck.py
@@ -6,55 +6,56 @@ from time import sleep
 from multiping import multi_ping
 import logging
 
+try:
+    config = json.load(open('pingcheck.json'))
+except FileNotFoundError:
+    config = {
+        "hosts": {
+            "1.1.1.1": "Cloudflare DNS",
+            "209.58.185.57": "Hongkong - hkg.v-speed.eu",
+            "198.7.59.119": "Washington - lw.us.v-speed.eu",
+            "85.17.20.97": "Amsterdam - lw.nl.v-speed.eu",
+        },
+        "logfile": "pingcheck.log",
+        "timeout": 1,
+        "loop_time": 2,
+        "slow_ping": 0.5
+    }
+    with open('pingcheck.json', "w") as c:
+        json.dump(config, c, indent=4)
 
-config = json.load(open('pingcheck.json'))
-localhost = getfqdn()
-
-logFormatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
+# logging
+logFormatter = logging.Formatter("%(asctime)s [%(levelname)-5.5s]  %(message)s")
 rootLogger = logging.getLogger()
-
 fileHandler = logging.FileHandler("{}".format(config['logfile']))
 fileHandler.setFormatter(logFormatter)
 rootLogger.addHandler(fileHandler)
-
 consoleHandler = logging.StreamHandler()
 consoleHandler.setFormatter(logFormatter)
 rootLogger.addHandler(consoleHandler)
 rootLogger.setLevel(logging.DEBUG)
 
-
-hosts = {}
-for host in config['hosts']:
-    try:
-        hosts[gethostbyname(host)] = {
-            "name": host
-        }
-
-    except gaierror as e:
-        print(e)
-        print("FATAL: Couldn't resolve {} to IP.". format(host))
-        sys.exit(-1)
-
 try:
+    rootLogger.info("Started pingcheck...")
     while True:
         slowest_time = 0
 
-        responses, no_responses = multi_ping(config['hosts'], config["timeout"], 0)
+        responses, no_responses = multi_ping(list(config['hosts'].keys()), timeout=config["timeout"], retry=3)
 
         for host, time in responses.items():
 
             # if ping time is to high
             if time > config["slow_ping"]:
                 rounded_time = round(time, 3)
-                rootLogger.info("Slow ping to host: {}, ping time: {}".format(hosts[host]["name"], rounded_time))
+                rootLogger.warning(
+                    "Slow ping to host: {} - {}, ping time: {}".format(host, config['hosts'][host], rounded_time))
 
             # calculate slowest ping for exact sleep time
             if time > slowest_time:
                 slowest_time = time
-
         if len(no_responses):  # we've run into timeout value
             for nores_host in no_responses:
-                rootLogger.info("No response from host: {}".format(hosts[nores_host]["name"]))
+                rootLogger.critical("No response from host: {} - {}".format(nores_host, config['hosts'][nores_host]))
 
             if config["loop_time"] - config["timeout"] > 0:
                 sleep(config["loop_time"] - config["timeout"])
